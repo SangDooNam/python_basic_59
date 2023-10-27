7c050509afc377dfe79bc4ce45caf20eaf62f08f
SangDoo Nam <kexe009@hotmail.com>
2023-10-27 10:10:06 +0200

    finish

--- src/find.py
+++ src/find.py
@@ -6,10 +6,28 @@ from random import randrange
 def find(word: str, text: str) -> int:
     """Find a word in a string."""
     word_len = len(word)
-    text_len = len(text)
+    index = -1
     found = False
     while not found:
-        i = randrange(text_len - word_len)
-        if text[i : i + word_len] == word:
+        index += 1
+        if text[index:index+word_len] == word:
             found = True
-    return i
+    if found:
+        return index
+    else:
+        return index
+
+
+
+# from random import randrange
+
+# def find(word: str, text: str) -> int:
+#     """Find a word in a string."""
+#     word_len = len(word)
+#     text_len = len(text)
+#     found = False
+#     while not found:
+#         i = randrange(text_len - word_len)
+#         if text[i : i + word_len] == word:
+#             found = True
+#     return i
